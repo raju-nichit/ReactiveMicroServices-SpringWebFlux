c7df5d63d40009809d0645005e7352396201b088
raju-nichit <meetrajnichit@gmail.com>
2024-03-23 21:12:07 +0530

    exception handling added for functional endpoint API

--- webflux-demo/src/main/java/com/shivay/webflux/config/RequestHandler.java
+++ webflux-demo/src/main/java/com/shivay/webflux/config/RequestHandler.java
@@ -2,6 +2,7 @@ package com.shivay.webflux.config;
 
 import com.shivay.webflux.dto.MultiplyRequestDTO;
 import com.shivay.webflux.dto.Response;
+import com.shivay.webflux.exception.InputFailedValidationException;
 import com.shivay.webflux.service.ReactiveMathService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -15,19 +16,19 @@ public class RequestHandler {
     @Autowired
     private ReactiveMathService mathService;
 
-    public Mono<ServerResponse> squareHandler(ServerRequest serverRequest){
-       int input = Integer.parseInt(serverRequest.pathVariable("input"));
+    public Mono<ServerResponse> squareHandler(ServerRequest serverRequest) {
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
 
-       return ServerResponse.ok().body(this.mathService.findSquare(input), Response.class);
+        return ServerResponse.ok().body(this.mathService.findSquare(input), Response.class);
     }
 
-    public Mono<ServerResponse> tableHandler(ServerRequest serverRequest){
+    public Mono<ServerResponse> tableHandler(ServerRequest serverRequest) {
         int input = Integer.parseInt(serverRequest.pathVariable("input"));
 
         return ServerResponse.ok().body(this.mathService.multiplicationTable(input), Response.class);
     }
 
-    public Mono<ServerResponse> tableStreamHandler(ServerRequest serverRequest){
+    public Mono<ServerResponse> tableStreamHandler(ServerRequest serverRequest) {
         int input = Integer.parseInt(serverRequest.pathVariable("input"));
 
         return ServerResponse
@@ -36,14 +37,25 @@ public class RequestHandler {
                 .body(this.mathService.multiplicationTable(input), Response.class);
     }
 
-    public Mono<ServerResponse> multiplyHandler(ServerRequest serverRequest){
+    public Mono<ServerResponse> multiplyHandler(ServerRequest serverRequest) {
 
         Mono<MultiplyRequestDTO> multiplyRequestDTOMono = serverRequest.bodyToMono(MultiplyRequestDTO.class);
 
         return ServerResponse
                 .ok()
 //                .contentType(MediaType.TEXT_EVENT_STREAM)
-                .body(this.mathService.multiply(multiplyRequestDTOMono),Response.class);
+                .body(this.mathService.multiply(multiplyRequestDTOMono), Response.class);
+    }
+
+
+    public Mono<ServerResponse> squareHandlerWithValidation(ServerRequest serverRequest) {
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+
+        if (input < 10 || input > 20) {
+            return Mono.error(new InputFailedValidationException(input));
+        }
+
+        return ServerResponse.ok().body(this.mathService.findSquare(input), Response.class);
     }
 
 }
--- webflux-demo/src/main/java/com/shivay/webflux/config/RouterConfig.java
+++ webflux-demo/src/main/java/com/shivay/webflux/config/RouterConfig.java
@@ -1,11 +1,17 @@
 package com.shivay.webflux.config;
 
+import com.shivay.webflux.dto.InputFailedValidationResponse;
+import com.shivay.webflux.exception.InputFailedValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.function.BiFunction;
 
 @Configuration
 public class RouterConfig {
@@ -22,7 +28,22 @@ public class RouterConfig {
                 .GET("router/table/{input}", requestHandler::tableHandler)
                 .GET("router/table-stream/{input}", requestHandler::tableStreamHandler)
                 .POST("router/multiply", requestHandler::multiplyHandler)
+                .GET("router/square/{input}/validation", requestHandler::squareHandlerWithValidation)
 
+                .onError(InputFailedValidationException.class,exceptionHandler())
                 .build();
     }
-}
+
+    private BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> exceptionHandler(){
+
+        return (err,req) -> {
+            InputFailedValidationException exception = (InputFailedValidationException) err;
+            InputFailedValidationResponse response = new InputFailedValidationResponse();
+
+            response.setInput(exception.getInput());
+            response.setMessage(exception.getMessage());
+
+            return ServerResponse.badRequest().bodyValue(response);
+         };
+    }
+ }
