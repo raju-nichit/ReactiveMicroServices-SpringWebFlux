d4270dc7c899ff5b690aa24ea2933647e507d48b
raju-nichit <meetrajnichit@gmail.com>
2024-03-25 12:14:53 +0530

    Webclient First test case added

---
+++ webflux-demo/src/test/java/com/shivay/webflux/BaseTest.java
@@ -0,0 +1,8 @@
+package com.shivay.webflux;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class BaseTest {
+
+}
