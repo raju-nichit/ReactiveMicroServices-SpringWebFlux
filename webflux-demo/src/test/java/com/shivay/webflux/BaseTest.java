bb85fd1b9406e2184481669fdcc9220cb703f99c
raju-nichit <meetrajnichit@gmail.com>
2024-03-25 16:59:03 +0530

    Flux endpoint Testing

--- webflux-demo/src/test/java/com/shivay/webflux/BaseTest.java
+++ webflux-demo/src/test/java/com/shivay/webflux/BaseTest.java
@@ -3,6 +3,6 @@ package com.shivay.webflux;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-class BaseTest {
+public class BaseTest {
 
 }
---
+++ webflux-demo/src/test/java/com/shivay/webflux/controller/MathServiceTest.java
@@ -0,0 +1,47 @@
+package com.shivay.webflux.controller;
+
+import com.shivay.webflux.BaseTest;
+import com.shivay.webflux.dto.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class MathServiceTest extends BaseTest {
+
+    @Autowired
+    private WebClient webClient;
+
+
+    @Test
+    public void squareEndpointTest(){
+
+
+        Mono<Response> response = this.webClient
+                .get()
+                .uri("reactive-math/square/{number}",5)
+                .retrieve()
+                .bodyToMono(Response.class);
+
+        StepVerifier.create(response)
+                .expectNextMatches(r-> r.getOutput() ==25)
+                .verifyComplete();
+    }
+
+    @Test
+    public void multiplyEndpointTest(){
+
+
+        Flux<Response> response = this.webClient
+                .get()
+                .uri("reactive-math/table/{input}",5)
+                .retrieve()
+                .bodyToFlux(Response.class);
+
+        StepVerifier.create(response)
+                .expectNextCount(10)
+                .verifyComplete();
+    }
+}
